136d80fb056247c89e5d626e8eb931d8e24d0d18
KaliShau <fa6248122@gmail.com>
2025-01-04 20:00:32 +0300

    feature: add widgets and config scripts

--- main.py
+++ main.py
@@ -7,9 +7,15 @@ from src.screens.sign_in import Ui_SignIn
 from src.screens.sign_up import Ui_SignUp
 from src.screens.create_ticket import Ui_CreateTicket
 from src.screens.my_create_tickets import Ui_MyCreateTickets
+from src.screens.my_assigned_tickets import Ui_MyAssignedTickets
+from src.screens.available_tickets import Ui_AvailableTickets
+from src.screens.db_config import Ui_DbConfig
 
 from src.functions.visible import  visible
 from src.functions.widgets import widgets
+from src.functions.config import config
+
+from src.functions.db_config_functions import db_config_functions
 
 class AppTracker(QMainWindow):
     def __init__(self):
@@ -17,17 +23,25 @@ class AppTracker(QMainWindow):
         self.main_window = Ui_MainWindow()
         self.main_window.setupUi(self)
 
+        # config
+        self.config = config()
+        self.config.create_config()
+
         # init widgets
         self.sign_in = Ui_SignIn()
         self.sign_up = Ui_SignUp()
         self.create_ticket = Ui_CreateTicket()
         self.my_create_tickets = Ui_MyCreateTickets()
+        self.my_assigned_tickets = Ui_MyAssignedTickets()
+        self.available_tickets = Ui_AvailableTickets()
+        self.db_config = Ui_DbConfig(self.config.load_config())
 
         # init functions
+        self.widgets = widgets(self.main_window, self.sign_in, self.sign_up, self.create_ticket, self.my_create_tickets, self.my_assigned_tickets, self.available_tickets, self.db_config)
         self.visible = visible(self.main_window)
-        self.widgets = widgets(self.main_window, self.sign_in, self.sign_up, self.create_ticket, self.my_create_tickets)
 
-
+        self.db_config_functions = db_config_functions(self.config, self.db_config)
+        
 
 
 
---
+++ src/functions/config.py
@@ -0,0 +1,36 @@
+import json
+import os
+
+class config:
+    def load_config(self):
+        with open('config.json', 'r') as file:
+            config = json.load(file)
+        return config
+
+    def save_config(self, password, user, host, port, db):
+        config = self.load_config()
+
+        config['database']['password'] = password
+        config['database']['user'] = user
+        config['database']['host'] = host
+        config['database']['port'] = port
+        config['database']['dbname'] = db
+
+        with open('config.json', 'w') as file:
+            json.dump(config, file, indent=5)
+        
+    def create_config(self):
+        if not os.path.exists('config.json'):
+            config = {
+            'database': {
+                'host': 'localhost',
+                'user': 'default_user',
+                'password': 'default_password',
+                'port': '3306',
+                'dbname': 'default_db'
+                }
+            }
+            with open('config.json', 'w') as file:
+                json.dump(config, file, indent=5)
+
+        
